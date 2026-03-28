288409595b888fa6d964f456b938498a61bbcbb0
Eduard Shavyrko <edshav@protonmail.com>
2026-03-28 01:32:05 -0300

    build: remove gh-pages

--- vite.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  // This sets the base URL path for the deployment to match your GitHub repository name
-  // base: "/renovation/",
-});
