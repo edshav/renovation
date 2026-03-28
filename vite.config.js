639782ca052169fb966857660882a0ab016e51f8
Eduard Shavyrko <edshav@protonmail.com>
2026-03-28 01:27:42 -0300

    feat: remove base URL configuration from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   // This sets the base URL path for the deployment to match your GitHub repository name
-  base: "/renovation/",
+  // base: "/renovation/",
 });
